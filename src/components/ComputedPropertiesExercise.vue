410ed5cdd4fd744412bbe127698afcec3867115f
Steffen Larson <steffman13@gmail.com>
2021-01-18 15:18:12 -0700

    finished the computed tab

--- src/components/ComputedPropertiesExercise.vue
+++ src/components/ComputedPropertiesExercise.vue
@@ -4,8 +4,8 @@
       <div class="col-12">
         <div class="border p-1">
           <div class="reverse-message">
-            <p>Original Message: {{}}</p>
-            <p>Computed Reversed Message: {{}}</p>
+            <p>Original Message: {{ state.message }}</p>
+            <p>Computed Reversed Message: {{ state.reversedMessage }}</p>
           </div>
         </div>
       </div>
@@ -19,13 +19,12 @@ import { computed, reactive } from "vue";
 export default {
   name: "computed-properties-exercise",
   setup() {
-    const state = reactive;
-    ({
+    const state = reactive({
       // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
       message: "Hello World!",
       reversedMessage: computed(() => {
         // all computed properties must return a value, what do you want this one to return?
-        return ""
+        return state.message.split("").reverse().join("");
       }),
     });
     return {
